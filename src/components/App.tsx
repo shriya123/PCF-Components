565a1975c8e852183ae7fa0d4fddbb948dd59f4d
Shriya Dhar <shriya2708@gmail.com>
2021-09-03 09:13:33 +0530

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+      Hi my name is Shriya
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'x-data-spreadsheet/dist/xspreadsheet.css';
+
+const Home = () => {
+  return <div>You've reached home</div>;
+};
+
+export default Home;
